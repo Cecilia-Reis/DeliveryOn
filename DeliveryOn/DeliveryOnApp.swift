b35fac1757a9ea90f5d14533daeaad035e143c9c
Cecilia Reis <cecilia.reis@ftdata.com.br>
2024-06-05 07:29:13 -0300

    Initial Commit

---
+++ DeliveryOn/DeliveryOnApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeliveryOnApp.swift
+//  DeliveryOn
+//
+//  Created by Cecilia Reis on 05/06/24.
+//
+
+import SwiftUI
+
+@main
+struct DeliveryOnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
