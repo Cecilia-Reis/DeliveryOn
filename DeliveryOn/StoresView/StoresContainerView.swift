0dcb903ebbf42520a87e7758ea71341b8ce2899d
Cecilia Reis <cecilia.reis@ftdata.com.br>
2024-06-11 10:12:03 -0300

    Criando a página de detalhes de um produto e adicionando filtros de estrelas e distância

--- DeliveryOn/StoresView/StoresContainerView.swift
+++ DeliveryOn/StoresView/StoresContainerView.swift
@@ -10,19 +10,96 @@ import SwiftUI
 struct StoresContainerView: View {
     
     let title = "Lojas"
+    @State private var ratingFilter = 0
+    @State private var minDistanceFilter:Double = 0
+    @State private var maxDistanceFilter:Double = 100
+    
+    var filteredStores: [StoreType] {
+        return storesMock.filter { store in
+            store.starts >= ratingFilter && (store.distance >= minDistanceFilter && store.distance < maxDistanceFilter)
+        }
+    }
+    
     
     var body: some View {
         VStack(alignment: .leading) {
-            Text(title)
-                .font(.headline)
-                .padding(20)
+            
+            HStack {
+                Text(title)
+                    .font(.headline)
+                    .padding(20)
+                
+                Spacer()
+                
+                HStack{
+                    Menu("Estrelas") {
+                        
+                        Button{
+                            ratingFilter = 0
+                        } label: {
+                            Text("Limpar filtro")
+                        }
+                        
+                        Divider()
+                        
+                        ForEach(1...5, id: \.self) { rating in
+                            Button{
+                                ratingFilter = rating
+                            } label: {
+                                if rating > 1 {
+                                    Text("\(rating) estrelas ou mais")
+                                } else {
+                                    Text("\(rating) estrela ou mais")
+                                }
+                                
+                            }
+                        }
+                    }
+                    .foregroundColor(.black)
+                    
+                    Menu("Distância") {
+                        
+                        Button{
+                            minDistanceFilter = 0
+                            maxDistanceFilter = 100
+                            
+                        } label: {
+                            Text("Limpar filtro")
+                        }
+                        
+                        Divider()
+                        
+                        ForEach(Array(stride(from: 0, to: 20, by: 5)), id:\.self) { distance in
+                            Button {
+                                minDistanceFilter = Double(distance)
+                                maxDistanceFilter = Double(distance + 5)
+                            } label: {
+                                Text("De \(distance) até \(distance + 5) km")
+                            }
+                        }
+                    }
+                }
+                
+                
+            }
             
             VStack(alignment: .leading, spacing: 30) {
-                ForEach(storesMock) { mock in
-                    NavigationLink{
-                        StoreDetailView(store: mock)
-                    } label: {
-                        StoresItemView(store: mock)
+                
+                if filteredStores.isEmpty {
+                    Text("Nenhum resultado encontrado.")
+                        .font(.title3)
+                        .bold()
+                        .foregroundColor(Color("ColorRed"))
+                        .padding(.vertical,32)
+                        .frame(maxWidth: .infinity)
+                } else {
+                    
+                    ForEach(filteredStores) { mock in
+                        NavigationLink{
+                            StoreDetailView(store: mock)
+                        } label: {
+                            StoresItemView(store: mock)
+                        }
                     }
                 }
             }
